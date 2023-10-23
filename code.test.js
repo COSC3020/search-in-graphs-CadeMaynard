7c8efcecf699a7e160438c76afdad6429e9e1f17
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-10-23 00:16:01 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,7 +3,7 @@ var test = require('unit.js');
 
 eval(fs.readFileSync('code.js')+'');
 
-        var searchMatrix1 = [[1,2],
+        var searchAdjList1 = [[1,2],
                             [3],
                             [1,4],
                             [],
@@ -13,17 +13,17 @@ eval(fs.readFileSync('code.js')+'');
         var search2Result = [0,2,4,5];
         var search3Result = [2];
 
-        var searchMatrix2 = [[1,3],
+        var searchAdjList2 = [[1,3],
                             [0,2],
                             [1,3],
                             [0,2]];
         var search4Result = [0,1,2];
 
         //General tests with a non-cyclical, directional graph
-        test.array(depthFirstSearch(searchMatrix1, 0, 3)) == search1Result;
-        test.value(depthFirstSearch(searchMatrix1, 0, 5)) == search2Result;
-        test.value(depthFirstSearch(searchMatrix1, 2, 2)) == search3Result;
+        test.array(depthFirstSearch(searchAdjList1, 0, 3)) == search1Result;
+        test.value(depthFirstSearch(searchAdjList1, 0, 5)) == search2Result;
+        test.value(depthFirstSearch(searchAdjList1, 2, 2)) == search3Result;
 
         //Tests with a non-directional graph to test that it does not loop infinitely
-        test.value(depthFirstSearch(searchMatrix1, 0, 2)) == search3Result;
+        test.value(depthFirstSearch(searchAdjList2, 0, 2)) == search3Result;
         
